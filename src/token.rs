60471ede1dc5f83f925e3ead0edf408578488644
rakin406 <rakinhacker@gmail.com>
2024-01-08 22:51:59 +0600

    refactor: Remove todo

--- src/token.rs
+++ src/token.rs
@@ -2,8 +2,6 @@ use std::any::Any;
 
 use crate::token_type::TokenType;
 
-// TODO: Add constructor and methods for Token.
-
 pub struct Token {
     pub token_type: TokenType,
     pub lexeme: &'static str,
