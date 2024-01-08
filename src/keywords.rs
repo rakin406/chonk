ede29bddeac7fcaa69227ae6463b355bc6ffb8e7
rakin406 <rakinhacker@gmail.com>
2024-01-08 23:45:30 +0600

    fix: Use phf library for compile time maps

--- src/keywords.rs
+++ src/keywords.rs
@@ -1,23 +1,23 @@
-use std::collections::HashMap;
+use phf::phf_map;
 
 use crate::token_type::TokenType;
 
 /// All the reserved words and it's token type.
-pub const KEYWORDS: HashMap<&str, TokenType> = HashMap::from([
-    ("null", TokenType::Null),
-    ("true", TokenType::True),
-    ("false", TokenType::False),
-    ("if", TokenType::If),
-    ("elif", TokenType::Elif),
-    ("else", TokenType::Else),
-    ("case", TokenType::Case),
-    ("default", TokenType::Default),
-    ("in", TokenType::In),
-    ("!in", TokenType::NotIn),
-    ("while", TokenType::While),
-    ("for", TokenType::For),
-    ("break", TokenType::Break),
-    ("continue", TokenType::Continue),
-    ("return", TokenType::Return),
-    ("echo", TokenType::Echo),
-]);
+pub const KEYWORDS: phf::Map<&str, TokenType> = phf_map! {
+    "null" => TokenType::Null,
+    "true" => TokenType::True,
+    "false" => TokenType::False,
+    "if" => TokenType::If,
+    "elif" => TokenType::Elif,
+    "else" => TokenType::Else,
+    "case" => TokenType::Case,
+    "default" => TokenType::Default,
+    "in" => TokenType::In,
+    "!in" => TokenType::NotIn,
+    "while" => TokenType::While,
+    "for" => TokenType::For,
+    "break" => TokenType::Break,
+    "continue" => TokenType::Continue,
+    "return" => TokenType::Return,
+    "echo" => TokenType::Echo,
+};
