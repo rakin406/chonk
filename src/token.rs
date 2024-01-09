d820cb3d88261e83dac696829342a3f011233a05
rakin406 <rakinhacker@gmail.com>
2024-01-09 12:14:02 +0600

    feat: Use String type

--- src/token.rs
+++ src/token.rs
@@ -4,7 +4,7 @@ use crate::token_type::TokenType;
 
 pub struct Token {
     pub token_type: TokenType,
-    pub lexeme: &'static str,
+    pub lexeme: String,
     pub literal: Option<Box<dyn Any>>,
     pub line: usize,
 }
