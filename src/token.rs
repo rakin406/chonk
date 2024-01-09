c2ea99f3dfc25bcbebbb40c1db4907f31aef14e2
rakin406 <rakinhacker@gmail.com>
2024-01-09 16:20:57 +0600

    feat: Add Literal enum

--- src/token.rs
+++ src/token.rs
@@ -1,10 +1,17 @@
-use std::any::Any;
-
 use crate::token_type::TokenType;
 
+#[derive(Debug, Clone)]
+pub enum Literal {
+    Identifier(String),
+    String(String),
+    Number(f64),
+}
+
+#[derive(Clone)]
 pub struct Token {
     pub token_type: TokenType,
     pub lexeme: String,
-    pub literal: Option<Box<dyn Any>>,
+    pub literal: Option<Literal>,
     pub line: usize,
+    pub column: i64,
 }
