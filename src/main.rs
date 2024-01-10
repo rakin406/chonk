6d805f14a9a552f46b6bf0b85df4cc609f089bdd
rakin406 <rakinhacker@gmail.com>
2024-01-10 19:03:53 +0600

    feat: Use clap library

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use clap::Parser;
+
 mod lexer;
 mod token;
 mod token_type;
 
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    /// Path of the script file to run
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello {}!", args.file)
 }
