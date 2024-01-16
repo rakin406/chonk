a28ecde08b49f66140e068cdea9fc25375bf9324
rakin406 <rakinhacker@gmail.com>
2024-01-16 12:04:37 +0600

    🐛 Remove .unwrap() to avoid moving out of shared reference

--- src/token.rs
+++ src/token.rs
@@ -28,11 +28,6 @@ impl Token {
 
     /// Returns the `Token` as a `String` format.
     pub fn to_string(&self) -> String {
-        format!(
-            "{:#?} {} {:#?}",
-            self.token_type,
-            self.lexeme,
-            self.literal.unwrap()
-        )
+        format!("{:#?} {} {:#?}", self.token_type, self.lexeme, self.literal)
     }
 }
