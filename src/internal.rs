be8f95aba247f18abcf1ec695e42302254748764
rakin406 <rakin406@users.noreply.github.com>
2024-01-20 08:01:36 +0000

    Apply rustfmt changes

--- src/internal.rs
+++ src/internal.rs
@@ -1,7 +1,7 @@
 pub mod ast_printer;
 pub mod error_reporter;
-pub mod token_type;
-pub mod token;
-pub mod parser;
 pub mod expr;
 pub mod lexer;
+pub mod parser;
+pub mod token;
+pub mod token_type;
