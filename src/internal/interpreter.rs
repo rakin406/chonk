0fcc790e047532ce30fa64bf3945254a7a177c33
rakin406 <rakinhacker@gmail.com>
2024-01-26 12:33:56 +0600

    feat: interpret binary and unary expressions

--- src/internal/interpreter.rs
+++ src/internal/interpreter.rs
@@ -2,7 +2,59 @@ use super::ast::{self, Visitor};
 use super::token::Literal;
 use super::token_type::TokenType;
 
-struct Interpreter {}
+struct Interpreter;
+
+impl Interpreter {
+    fn interpret_binary(&self, lhs: &ast::Expr, op: TokenType, rhs: &ast::Expr) -> Literal {
+        let left = self.visit_expr(lhs);
+        let right = self.visit_expr(rhs);
+
+        match (left, op, right) {
+            // TODO: Create a "Boolean" literal for comparison operators.
+            // (Literal::Number(n1), TokenType::Greater, Literal::Number(n2)) => {
+            //     return Literal::Number(n1 > n2);
+            // }
+            // (Literal::Number(n1), TokenType::GreaterEqual, Literal::Number(n2)) => {
+            //     return Literal::Number(n1 >= n2);
+            // }
+            // (Literal::Number(n1), TokenType::Less, Literal::Number(n2)) => {
+            //     return Literal::Number(n1 < n2);
+            // }
+            // (Literal::Number(n1), TokenType::LessEqual, Literal::Number(n2)) => {
+            //     return Literal::Number(n1 <= n2);
+            // }
+            (Literal::Number(n1), TokenType::Minus, Literal::Number(n2)) => {
+                return Literal::Number(n1 - n2);
+            }
+            (Literal::Number(n1), TokenType::Plus, Literal::Number(n2)) => {
+                return Literal::Number(n1 + n2);
+            }
+            (Literal::String(s1), TokenType::Plus, Literal::String(s2)) => {
+                return Literal::String(s1 + &s2);
+            }
+            (Literal::Number(n1), TokenType::Slash, Literal::Number(n2)) => {
+                return Literal::Number(n1 / n2);
+            }
+            (Literal::Number(n1), TokenType::Star, Literal::Number(n2)) => {
+                return Literal::Number(n1 * n2);
+            }
+            _ => Literal::Null,
+        }
+    }
+
+    fn interpret_unary(&self, op: TokenType, rhs: &ast::Expr) -> Literal {
+        let right = self.visit_expr(rhs);
+
+        match (op, &right) {
+            (TokenType::Minus, Literal::Number(value)) => Literal::Number(-value),
+            (TokenType::Bang, _) => match is_truthy(right) {
+                true => Literal::False(false),
+                false => Literal::True(true),
+            },
+            _ => Literal::Null,
+        }
+    }
+}
 
 impl Visitor<Literal> for Interpreter {
     fn visit_expr(&self, expr: &ast::Expr) -> Literal {
@@ -10,45 +62,12 @@ impl Visitor<Literal> for Interpreter {
 
         match expr {
             Expr::BoolOp(op, rhs) => todo!(),
-            Expr::BinOp(lhs, op, rhs) => {
-                let left = self.visit_expr(lhs);
-                let right = self.visit_expr(rhs);
-
-                match op.ty {
-                    // TODO: I seriously need to change my design and code
-                    // structure. I need so many match statements here that it's
-                    // actually crazy.
-                    TokenType::Minus => {}
-                    TokenType::Slash => {}
-                    TokenType::Star => {}
-                    _ => {}
-                }
-
-                return Literal::Null;
-            }
-            Expr::UnaryOp(op, rhs) => {
-                let right = self.visit_expr(rhs);
-
-                match right {
-                    Literal::Number(value) => match op.ty {
-                        TokenType::Minus => return Literal::Number(-value),
-                        _ => {}
-                    },
-                    _ => match op.ty {
-                        TokenType::Bang => match is_truthy(right) {
-                            true => return Literal::False(false),
-                            false => return Literal::True(true),
-                        },
-                        _ => {}
-                    },
-                }
-
-                return Literal::Null;
-            }
+            Expr::BinOp(lhs, op, rhs) => self.interpret_binary(lhs, op.ty, rhs),
+            Expr::UnaryOp(op, rhs) => self.interpret_unary(op.ty, rhs),
             Expr::Grouping(e) => self.visit_expr(e),
             Expr::Logical(lhs, op, rhs) => todo!(),
             Expr::Call { func, args } => todo!(),
-            Expr::Constant(literal) => return literal.to_owned(),
+            Expr::Constant(literal) => literal.to_owned(),
             Expr::Variable(token) => todo!(),
         }
     }
@@ -57,8 +76,8 @@ impl Visitor<Literal> for Interpreter {
 /// Returns `true` if the literal is "truthy".
 fn is_truthy(literal: Literal) -> bool {
     match literal {
-        Literal::Null => return false,
-        Literal::True(value) | Literal::False(value) => return value,
-        _ => return true,
+        Literal::Null => false,
+        Literal::True(value) | Literal::False(value) => value,
+        _ => true,
     }
 }
