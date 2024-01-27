3e6b601b9a4500e7548ee2fb5bb52e528ee00f12
rakin406 <rakinhacker@gmail.com>
2024-01-28 00:26:08 +0600

    feat: panic on token error

--- src/internal/environment.rs
+++ src/internal/environment.rs
@@ -1,36 +1,42 @@
 use std::collections::HashMap;
 
-use super::token::Literal;
+use super::error_reporter::{ErrorReporter, ErrorType};
+use super::token::{Literal, Token};
 
 #[derive(Default)]
 pub struct Environment {
-    values: HashMap<String, Literal>,
+    store: HashMap<String, Literal>,
 }
 
 impl Environment {
     /// Binds a new name to a value.
     pub fn define(&mut self, name: String, value: Literal) {
-        self.values.insert(name, value);
+        self.store.insert(name, value);
     }
 
     /// Assigns a new value to an existing name.
-    pub fn assign(&mut self, name: String, value: Literal) {
-        if self.values.contains_key(&name) {
-            self.values.insert(name, value);
+    pub fn assign(&mut self, name: Token, value: Literal) {
+        if self.store.contains_key(&name.lexeme) {
+            self.store.insert(name.lexeme, value);
+        } else {
+            self.token_error(name, &format!("Undefined variable \"{}\"", name.lexeme));
+            panic!();
         }
     }
 
     /// Returns the literal value bound to the name.
-    pub fn get(&self, name: String) -> Option<Literal> {
-        if let Some(value) = self.values.get(&name) {
-            return Some(value.to_owned());
+    pub fn get(&self, name: Token) -> Literal {
+        if let Some(value) = self.store.get(&name.lexeme) {
+            return value.to_owned();
         }
 
-        // NOTE: I should return a RuntimeError here instead of returning None.
-        // Because of laziness and keeping things simple, I am not focusing much
-        // on errors so that the project is finished quickly. Once the language
-        // is working, I will add more error support.
-
-        None
+        // NOTE: This looks ugly to me, maybe turn ErrorReporter into ErrorFormatter?
+        // That way I could pass in the error string to panic!().
+        self.token_error(name, &format!("Undefined variable \"{}\"", name.lexeme));
+        panic!();
     }
+}
+
+impl ErrorReporter for Environment {
+    const ERROR_TYPE: ErrorType = ErrorType::RuntimeError;
 }
