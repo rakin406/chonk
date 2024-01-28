bf252581c695ce122523aa81ffaf9c59d7b0a122
rakin406 <rakinhacker@gmail.com>
2024-01-28 16:43:24 +0600

    feat: add outer field and constructor

--- src/internal/environment.rs
+++ src/internal/environment.rs
@@ -6,13 +6,23 @@ use super::token::{Literal, Token};
 #[derive(Default)]
 pub struct Environment {
     store: HashMap<String, Literal>,
+    outer: Option<Box<Environment>>,
 }
 
 impl Environment {
+    /// Creates a new outer scope.
+    pub fn new_outer(&mut self, outer: Box<Environment>) {
+        self.outer = Some(outer);
+    }
+
     /// Returns the literal value bound to the name.
     pub fn get(&self, name: &Token) -> Literal {
         if let Some(value) = self.store.get(&name.lexeme) {
             return value.to_owned();
+        }
+
+        if let Some(outer_env) = &self.outer {
+            return outer_env.get(name);
         }
 
         self.token_error(
